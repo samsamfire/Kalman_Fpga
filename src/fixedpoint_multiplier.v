0836648cb605b3a6dd88c021ae34ffb251ef2488
sam <samfire.pilote@gmail.com>
2020-04-25 18:37:18 +0530

    Revert "Corrected double driver for temp_result"
    This reverts commit 73fa1f3fdf0a5b22dc22e6a43d0ceeff71dc1d04.

--- src/fixedpoint_multiplier.v
+++ src/fixedpoint_multiplier.v
@@ -38,6 +38,7 @@ always @(a,b) begin
 
     multiplier_ina = a_inv;
     multiplier_inb = b;
+    temp_result = - result_absolute_32bit;
 
   end
 
@@ -45,6 +46,7 @@ always @(a,b) begin
   else if(!sign_a && sign_b) begin
     multiplier_ina = a;
     multiplier_inb = b_inv;
+    temp_result = - result_absolute_32bit;
 
   end
 
@@ -52,17 +54,20 @@ always @(a,b) begin
   else if(sign_a && sign_b) begin
     multiplier_ina = a_inv;
     multiplier_inb = b_inv;
+    temp_result = result_absolute_32bit;
   end
 
   //If a and b are both positive
   else if(!sign_a && !sign_b)begin
     multiplier_ina = a;
     multiplier_inb = b;
+    temp_result = result_absolute_32bit;
   end
 
   else begin
     multiplier_ina = a;
     multiplier_inb = b;
+    temp_result = result_absolute_32bit;
   end
   
   
@@ -93,10 +98,10 @@ always @(result_absolute_32bit) begin
   end
   //Check for overflow, overflow happens if the upper bits of result_absolute are non-zero
   
-  if(result_absolute[2*N-1:N-1+Q] != 0)
-       ovr = 1'b1;
-    else
-       ovr = 1'b0;
+   // if(result_absolute[2*N-1:N-1+Q] != 0)
+   //    ovr = 1'b1;
+   // else
+   //   ovr = 1'b0;
   
 end
 
